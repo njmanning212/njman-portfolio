1e1bd41f8dca5239e9d21061d8da702a6f16f165
Nicholas Manning <njmanning212@gmail.com>
2023-06-08 14:15:28 -0500

    create type effect for roles

---
+++ js/app.js
@@ -0,0 +1,45 @@
+/*------------ Constants ------------*/
+const roles = ['Software Engineer', 'Full Stack Developer', 'Problem Solver', 'Husband', 'Father'];
+
+/*------------ Variables ------------*/
+let index = 0;
+let deleting = false;
+
+/*---- Cached Element References ----*/
+const rolesEl = document.getElementById('roles');
+
+/*------------ Functions ------------*/
+init();
+
+function init() {
+  typeText();
+}
+
+function typeText() {
+  const role = roles[index % roles.length];
+  const currentRole = rolesEl.textContent;
+
+  if (deleting) {
+    if (currentRole.length > 0) {
+      rolesEl.textContent = currentRole.slice(0, -1);
+      setTimeout(typeText, 100);
+    } else {
+      deleting = false;
+      index = (index + 1) % roles.length;
+      setTimeout(typeText, 500);
+    }
+  } else {
+    if (currentRole !== role) {
+      if (currentRole.length < role.length) {
+        rolesEl.textContent = role.slice(0, currentRole.length + 1);
+        setTimeout(typeText, 100);
+      } else {
+        deleting = true;
+        setTimeout(typeText, 1000);
+      }
+    } else {
+      deleting = true;
+      setTimeout(typeText, 1000);
+    }
+  }
+}
